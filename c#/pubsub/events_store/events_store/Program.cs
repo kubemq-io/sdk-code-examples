f07cb5cc505056a4fea5140eb8f617f7f52b8e9c
Lior Nabat <lior.nabat@gmail.com>
2024-01-27 09:09:54 +0200

    Add C# PubSub project's dependencies configuration
    The events.deps.json file has been added to specify all the dependencies and their versions required for the .NET Core 7 runtime of the C# PubSub project. This includes dependencies such as Google Protobuf, Grpc.Core, and many from Microsoft.Extensions., ensuring the project functions correctly with proper versioning.

---
+++ c#/pubsub/events_store/events_store/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using KubeMQ.Grpc;
+using KubeMQ.SDK.csharp.QueueStream;
+
+namespace events_store
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var  subscriber = new KubeMQ.SDK.csharp.Events.Subscriber("localhost:50000");
+            try
+            {
+                subscriber.SubscribeToEvents(new KubeMQ.SDK.csharp.Subscription.SubscribeRequest
+                    {
+                        Channel = "es1",
+                        SubscribeType = KubeMQ.SDK.csharp.Subscription.SubscribeType.EventsStore,
+                        EventsStoreType = KubeMQ.SDK.csharp.Subscription.EventsStoreType.StartNewOnly,
+                        ClientID = "subscriber-1",
+                    }, (eventReceive) =>
+                    {
+                        Console.WriteLine($"Event Store Received: EventID:{eventReceive.EventID} Body:{ System.Text.Encoding.UTF8.GetString(eventReceive.Body)} ");
+                    },
+                    (errorHandler) =>                 
+                    {
+                        Console.WriteLine(errorHandler.Message);
+                    });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            await Task.Delay(1000);
+
+            var sender = new KubeMQ.SDK.csharp.Events.Sender("localhost:50000");
+            try
+            {
+                var result = sender.SendEvent(new KubeMQ.SDK.csharp.Events.Event()
+                {                  
+                    Channel = "es1",
+                    Body = "hello kubemq - sending an event store message"u8.ToArray(),
+                    Store = true
+                });
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);          
+            }
+            await Task.Delay(1000);
+        }
+    }
+}
