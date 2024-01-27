5cbde31fc234259071767116c46a373b82e5b187
Lior Nabat <lior.nabat@gmail.com>
2024-01-27 07:58:16 +0200

    Add basic.deps.json for .NETCoreApp, Version=v7.0
    This commit introduces the basic.deps.json file which is a part of a .NET Core app deployment. The JSON file includes dependencies, runtime and compilation options for .NETCoreApp v7.0. The file is crucial for the runtime to understand the dependencies of the application and how to load them.

---
+++ c#/queues/basic/basic/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using KubeMQ.Grpc;
+using KubeMQ.SDK.csharp.QueueStream;
+
+namespace basic
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            QueueStream client = new QueueStream("localhost:50000", "some-client-Id");
+            await Task.Delay(1000);
+            try
+            {
+                List<Message> messages = new List<Message>();
+                messages.Add( new Message()
+                {
+                    Queue = "q1",
+                    Body = System.Text.Encoding.UTF8.GetBytes("hello kubemq - sending single message")
+                });
+
+                await client.Send(new SendRequest(messages));
+                Console.WriteLine("Message sent");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            try
+            {
+                PollRequest pollRequest = new PollRequest()
+                {
+                    Queue = "q1",
+                    WaitTimeout = 1000,
+                    MaxItems = 1,
+                    AutoAck = true,
+                };
+                PollResponse response = await client.Poll(pollRequest);
+                Console.WriteLine($"Messages Received: {response.Messages.Count}");
+                foreach (Message message in response.Messages)
+                {
+                    string bodyAsString = System.Text.Encoding.UTF8.GetString(message.Body);
+                    Console.WriteLine($"MessageID:{message.MessageID}, Body:{bodyAsString}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
